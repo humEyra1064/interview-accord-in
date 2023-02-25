5df8c543a1a43c942b2dc2a517783cce11f12ed5
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-25 12:08:58 +0300

    qestion added

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,15 +1,25 @@
-import {arrowup} from "../helper/icons"
+import {arrowup,arrowdown} from "../helper/icons"
 import {useState} from "react"
 const Question = ({id,question,answer}) => {
     const [show, setShow] = useState(false)
   return (
     <div className="card-group">
-      {show ? <div className="card">
+      {show ? 
+      
+     ( <div className="card">
         <div className="ques-answer">
-            <h5>{id}.{question}</h5>
-            <button>{arrowup}</button>
+            <h5> {id}.{question}</h5>
+            <button onClick={()=>setShow(false)}  >{arrowup}</button>
         </div>
-      </div> :  }
+        <p>{answer}</p>
+      </div> ):(
+      <div className="card">
+      <div className="ques-answer">
+          <h5>{id}.{question}</h5>
+          <button onClick={()=>setShow(true)}>{arrowdown}</button>
+      </div>
+     </div>
+        )}
     </div>
   )
 }
